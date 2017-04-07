f251fa4043e1eed928d252c671bd2178f400b4e3
Cedric LOMBARDOT <cedric.lombardot@gmail.com>
2017-04-07 12:29:37 +0200

    Connect to cloud

---
+++ tools/aws.js
@@ -0,0 +1,75 @@
+// import entire SDK 
+const AWS = require('aws-sdk');
+const fs = require('fs');
+var crypto = require('crypto');
+
+
+var AWSRekognize = function(config) {
+
+    AWS.config.update(config.aws.credentials);
+
+    var _upload = function(imagePath, done) {
+        var s3 = new AWS.S3();
+        var s3Key = "img-"+crypto.randomBytes(4).readUInt32LE(0);
+
+        fs.readFile(imagePath, (err, data) => {
+
+            if (err) { throw err; }
+
+            var base64data = new Buffer(data, 'binary');
+
+            var s3 = new AWS.S3();
+            s3.putObject({
+                Bucket: config.aws.s3.bucketName,
+                Key: s3Key,
+                Body: base64data,
+                ACL: 'public-read'
+            }, function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+
+                done(null, s3Key);
+            });
+        });
+    }
+
+    var _detectFaces = function(s3Key, done) {
+        var reko = new AWS.Rekognition();
+        reko.detectFaces({
+            Image: {
+                S3Object: {
+                    Bucket: config.aws.s3.bucketName,
+                    Name: s3Key
+                }
+            }
+        }, function(err, data) {
+            if (err) {
+                return done(err, err.stack);
+            }
+
+            done(null, data);
+        });
+    }
+
+    return  {
+        rekognize: function(imagePath, done) {
+            _upload(imagePath, (err, s3Key) => {
+                if (err) {
+                    return done(err);
+                }
+
+                _detectFaces(s3Key, (err, infos) => {
+                    if (err) {
+                        return done(err, infos);
+                    }
+
+                    return done(null, infos);
+                });
+            });
+        }
+    }
+}
+
+
+module.exports = AWSRekognize;
---
+++ tools/gvision.js
@@ -0,0 +1,41 @@
+const vision = require('node-cloud-vision-api');
+
+var GoogleVision = function(config) {
+
+    vision.init({
+        auth: config.google.credentials.api_key
+    });
+
+    var _rekognize = function(imagePath, done) {
+        const visionRequest = new vision.Request({
+            image: new vision.Image(imagePath),
+            features: [
+                new vision.Feature('FACE_DETECTION', 4),
+                new vision.Feature('LABEL_DETECTION', 10),
+                new vision.Feature('LOGO_DETECTION', 3),
+                new vision.Feature('LANDMARK_DETECTION', 10),
+                new vision.Feature('TEXT_DETECTION', 30)
+            ]
+        });
+
+        vision.annotate(visionRequest).then((visonResponse) => {
+            done(null, visonResponse.responses);
+        }, (e) => {
+            done(e);
+        });
+    };
+
+    return  {
+        rekognize: function(imagePath, done) {
+            return _rekognize(imagePath, done);
+        }
+    }
+};
+
+module.exports = GoogleVision;
+
+
+
+  
+
+  
